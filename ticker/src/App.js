9c1697b2bd6ffcd3aff85adc83b28fc13dbf84ce
unknown <dongtu@gmail.com>
2020-03-11 20:24:57 -0400

    added the componentDidMount method and shouldComponentUpdate

--- ticker/src/App.js
+++ ticker/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
 
-export default App;
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1});
+    },1000);
+  };
+
+  shouldComponentUpdate(nextProps, nextState){
+    if (nextState.count % 3 === 0)
+      return true;
+    else return false;
+
+  }
+
+    render (){
+      return (
+        <div>
+        <h1>The ticker is: <span>{this.state.count}</span></h1>
+
+        <button type="button" onClick={() => this.setState({count:0})}>Reset the Ticker</button>
+        </div>
+      );
+    };
+};
+
+export default Ticker;
